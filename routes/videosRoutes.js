385268f1e9a8a846e44e0183243ab7120e62ace9
Chao Meng <cmeng036@gmail.com>
2024-01-30 22:33:35 -0500

    added /videos and /videos/:videoId route and passed test for server and client

--- routes/videosRoutes.js
+++ routes/videosRoutes.js
@@ -1,11 +1,27 @@
 const express=require("express");
 const videosRouter=express.Router();
 const fs=require("fs");
-const {v4:uuidv4}=require("uuid");
+//const {v4:uuidv4}=require("uuid");
 const FILE_PATH="./data/videosData.json";
 
 const readVideos=()=>{
     const videosData=fs.readFileSync(FILE_PATH);
     const parseVideos=JSON.parse(videosData);
-    return parseVideos;
+    //only return the videoDetails
+    return parseVideos.videoDetails;
 }
+videosRouter.get("/",(req,res)=>{
+    const videosData=readVideos();
+    res.status(200).json(videosData);
+})
+videosRouter.get("/:videoId",(req,res)=>{
+    const videosData=readVideos();
+    const singleVideo=videosData.find((videoData)=>videoData.id===req.params.videoId);
+    if (singleVideo) {
+        res.json(singleVideo);
+    } else {
+        res.status(404).send('No video with that id exists');
+    }
+})
+videosRouter.post("")
+module.exports=videosRouter;
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
 const express=require("express");
 const app=express();
-
+const videosRoutes=require("./routes/videosRoutes.js")
 const cors=require("cors");
 const PORT=process.env.PORT||8081;
 //allow cross-origin requests and is enabled for all sources by default
 app.use(cors());
-//app.use("/videos",videosRoutes);
+app.use("/videos",videosRoutes);
 app.listen(PORT,()=>{
     console.log(`Listening on PORT ${PORT}`);
 })
